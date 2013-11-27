ac3690f7ac86c1adfad70926bbcad97c6fd2434e
Richard.of.Shaftesbury@gmail.com <Richard.of.Shaftesbury@gmail.com@052c0c06-60dd-4d73-2e48-45615e194d6a>
2013-11-27 22:32:50 +0000

    Added some more tests for the LispList

--- Functional-Utils-Java/src/test/java/me/shaftesbury/utils/functional/LispListTest.java
+++ Functional-Utils-Java/src/test/java/me/shaftesbury/utils/functional/LispListTest.java
@@ -25,6 +25,76 @@ public class LispListTest
     public void tailTest1()
     {
         List<Integer> l = list(1,list(2,LispList.<Integer>nil()));
-        Assert.assertEquals(list(2,LispList.<Integer>nil()), l.tail());
+        Assert.assertEquals(list(2,LispList.nil()), l.tail());
+    }
+
+    @Test
+    public void mapTest1()
+    {
+        List<Integer> input = list(1, list(2, list(3, list(4, list(5, LispList.<Integer>nil())))));
+        List<String> output = map(Functional.dStringify, input);
+        Assert.assertEquals(list("1",list("2",list("3",list("4",list("5",LispList.nil()))))),output);
+    }
+
+    @Test
+    public void filterTest1()
+    {
+        List<Integer> input = list(2,list(4,list(6,list(8,list(10,LispList.<Integer>nil())))));
+        List<Integer> oddElems = filter(Functional.IsOdd, input);
+        Assert.assertEquals(LispList.<Integer>nil(), oddElems);
+    }
+
+    @Test
+    public void filterTest2()
+    {
+        List<Integer> input = list(2,list(5,list(7,list(8,list(10,LispList.<Integer>nil())))));
+        List<Integer> evenElems = filter(Functional.IsEven, input);
+
+        List<Integer> expected = list(2,list(8,list(10,LispList.<Integer>nil())));
+        Assert.assertEquals(expected, evenElems);
+    }
+
+    @Test
+    public void filterTest3()
+    {
+        List<Integer> input = list(2,list(4,list(6,list(8,list(10,LispList.<Integer>nil())))));
+        final Integer limit = 5;
+        List<Integer> highElems = filter(
+                new Func<Integer,Boolean>()
+                {
+                    @Override
+                    public Boolean apply(Integer a) { return a > limit;}
+                }, input);
+
+        List<Integer> expected = list(6,list(8,list(10,LispList.<Integer>nil())));
+        Assert.assertEquals(expected, highElems);
+    }
+
+    @Test
+    public void filterTest4()
+    {
+        List<Integer> input = list(2,list(4,list(6,list(8,list(10,LispList.<Integer>nil())))));
+        final Integer limit = 10;
+        List<Integer> output = filter(
+                new Func<Integer,Boolean>()
+                {
+                    @Override public Boolean apply(Integer a) {return a > limit;}
+                }, input);
+
+        Assert.assertTrue(output.isEmpty());
+    }
+
+    @Test
+    public void filterTest5()
+    {
+        List<Integer> input = list(2,list(4,list(6,list(8,list(10,list(12,list(14,list(16,list(18,list(20,LispList.<Integer>nil()))))))))));
+        List<Integer> expected = list(4,list(8,list(12,list(16,list(20,LispList.<Integer>nil())))));
+        List<Integer> output = filter(
+                new Func<Integer,Boolean>()
+                {
+                    @Override public Boolean apply(Integer a) {return a % 4 ==0;}
+                }, input);
+
+        Assert.assertEquals(expected, output);
     }
 }
