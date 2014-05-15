9689cb4ac377f87b6b695c78e60ed5b2428bd4f2
Richard.of.Shaftesbury@gmail.com <Richard.of.Shaftesbury@gmail.com@052c0c06-60dd-4d73-2e48-45615e194d6a>
2014-05-15 12:59:37 +0000

    Add examples of extended generic functions, that is to say operations on containers of heterogeneous types

---
+++ Functional-Utils-Java/src/test/java/me/shaftesbury/utils/functional/ExtendedGenericsFunctionalTest.java
@@ -0,0 +1,58 @@
+package me.shaftesbury.utils.functional;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by Bob on 13/02/14.
+ */
+public final class ExtendedGenericsFunctionalTest
+{
+    @Test
+    public final void chooseTestParseStringToNumber()
+    {
+        final Func<String,Option<Number>> toNumber = new Func<String, Option<Number>>() {
+            @Override
+            public Option<Number> apply(String s) {
+                try {
+                    if(s.contains("."))// then it's a float
+                        return Option.<Number>toOption(Double.parseDouble(s));
+                    else return Option.<Number>toOption(Integer.parseInt(s));
+                } catch(final NumberFormatException t) {
+                    return Option.None();
+                }
+            }
+        };
+
+        final List<Number> expected = Arrays.<Number>asList(new Integer(1), new Integer(2), new Double(4.5));
+        final List<Number> output = Functional.choose(toNumber, Arrays.asList("1","2","at","4.5"));
+
+        Assert.assertArrayEquals(expected.toArray(),output.toArray());
+    }
+
+    @Test
+    public final void chooseTestParseObjectToNumber()
+    {
+        final Func<Object,Option<Number>> toNumber = new Func<Object, Option<Number>>() {
+            @Override
+            public Option<Number> apply(Object o) {
+                final String s = o.toString();
+                try {
+                    if(s.contains("."))// then it's a float
+                        return Option.<Number>toOption(Double.parseDouble(s));
+                    else return Option.<Number>toOption(Integer.parseInt(s));
+                } catch(final NumberFormatException t) {
+                    return Option.None();
+                }
+            }
+        };
+
+        final List<Number> expected = Arrays.<Number>asList(new Integer(1), new Integer(2), new Double(4.5), new Double(4.5), new Integer(1), new Integer(10));
+        final List<Number> output = Functional.choose(toNumber, Arrays.<Object>asList("1","2","at","4.5",4.5,1,new Integer(10)));
+
+        Assert.assertArrayEquals(expected.toArray(),output.toArray());
+    }
+}
