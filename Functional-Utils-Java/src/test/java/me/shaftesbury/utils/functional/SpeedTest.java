3243209a82b0c9ec03a9dc7187804ab338af5bbf
Richard.of.Shaftesbury@gmail.com <Richard.of.Shaftesbury@gmail.com@052c0c06-60dd-4d73-2e48-45615e194d6a>
2014-09-16 12:29:41 +0000

    Speed tests used to determine whether we should perform optimisations if the input type of the algorithm has a known size even for filter-type of transformations

---
+++ Functional-Utils-Java/src/test/java/me/shaftesbury/utils/functional/SpeedTest.java
@@ -0,0 +1,183 @@
+package me.shaftesbury.utils.functional;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class SpeedTest
+{
+    public final static <A,B> List<B> mapWithIterable(final Func<A, ? extends B> f, final Iterable<? extends A> input)
+    {
+        final List<B> output = new ArrayList<B>();
+        for(final A a : input)
+            output.add(f.apply(a));
+        return Collections.unmodifiableList(output);
+    }
+
+    public final static <A,B> List<B> mapWithCollection(final Func<A, ? extends B> f, final Collection<? extends A> input)
+    {
+        final List<B> output = new ArrayList<B>(input.size());
+        for(final A a : input)
+            output.add(f.apply(a));
+        return Collections.unmodifiableList(output);
+    }
+
+    public final static <A,B> List<B> mapWithInstanceOf(final Func<A, ? extends B> f, final Iterable<? extends A> input)
+    {
+        final List<B> output = input instanceof Collection<?> ? new ArrayList<B>(((Collection) input).size()) : new ArrayList<B>();
+        for(final A a : input)
+            output.add(f.apply(a));
+        return Collections.unmodifiableList(output);
+    }
+
+
+    @Test
+    public void MapTest1()
+    {
+        final int howMany=10;
+        final Collection<Integer> input = Functional.init(Functional.constant(10),1000000);
+        {
+            final long start = System.currentTimeMillis();
+            for (int i = 0; i < howMany; ++i) {
+                final Collection<String> output = mapWithIterable(Functional.<Integer>dStringify(), input);
+            }
+            final long howLong = System.currentTimeMillis() - start;
+            System.out.println(howLong + "ms to map using Iterable and whatever array resizing the JVM implements");
+        }
+        {
+            final long start = System.currentTimeMillis();
+            for (int i = 0; i < howMany; ++i) {
+                final Collection<String> output = mapWithCollection(Functional.<Integer>dStringify(), input);
+            }
+            final long howLong = System.currentTimeMillis() - start;
+            System.out.println(howLong + "ms to map using Collection");
+        }
+        {
+            final long start = System.currentTimeMillis();
+            for (int i = 0; i < howMany; ++i) {
+                final Collection<String> output = mapWithInstanceOf(Functional.<Integer>dStringify(), input);
+            }
+            final long howLong = System.currentTimeMillis() - start;
+            System.out.println(howLong + "ms to map using a Collection and instanceof to determine the initial array size");
+        }
+    }
+
+    private static final Func<Integer,Integer> DoublingGenerator =
+            new Func<Integer,Integer>()
+            {
+                @Override public Integer apply(Integer a) { return 2*a;}
+            };
+
+    public final static <A>List<A> filterWithIterable(final Func<? super A,Boolean> pred, final Iterable<A> input)
+    {
+        final List<A> output = new ArrayList<A>();
+        for(final A element : input)
+            if(pred.apply(element))
+                output.add(element);
+
+        return Collections.unmodifiableList(output);
+    }
+
+    public final static <A>List<A> filterWithCollection(final Func<? super A,Boolean> pred, final Collection<A> input)
+    {
+        final List<A> output = new ArrayList<A>(input.size());
+        for(final A element : input)
+            if(pred.apply(element))
+                output.add(element);
+
+        return Collections.unmodifiableList(output);
+    }
+
+    public final static <A>List<A> filterWithInstanceOf(final Func<? super A,Boolean> pred, final Iterable<A> input)
+    {
+        final List<A> output = input instanceof Collection<?> ? new ArrayList<A>(((Collection) input).size()) : new ArrayList<A>();
+        for(final A element : input)
+            if(pred.apply(element))
+                output.add(element);
+
+        return Collections.unmodifiableList(output);
+    }
+
+    @Test
+    public void filterTest1()
+    {
+        final int howMany=10;
+        final Collection<Integer> input = Functional.init(DoublingGenerator, 10000000);
+
+        {
+            final long start = System.currentTimeMillis();
+            for (int i = 0; i < howMany; ++i) {
+                final Collection<Integer> output = filterWithIterable(Functional.isOdd, input);
+            }
+            final long howLong = System.currentTimeMillis() - start;
+            System.out.println(howLong + "ms to filter using Iterable and whatever array resizing the JVM implements");
+        }
+        {
+            final long start = System.currentTimeMillis();
+            for (int i = 0; i < howMany; ++i) {
+                final Collection<Integer> output = filterWithCollection(Functional.isOdd, input);
+            }
+            final long howLong = System.currentTimeMillis() - start;
+            System.out.println(howLong + "ms to filter using Collection");
+        }
+        {
+            final long start = System.currentTimeMillis();
+            for (int i = 0; i < howMany; ++i) {
+                final Collection<Integer> output = filterWithInstanceOf(Functional.isOdd, input);
+            }
+            final long howLong = System.currentTimeMillis() - start;
+            System.out.println(howLong + "ms to filter using a Collection and instanceof to determine the initial array size");
+        }
+
+        final Collection<Integer> output1 = filterWithIterable(Functional.isOdd, input);
+        final Collection<Integer> output2 = filterWithCollection(Functional.isOdd, input);
+        final Collection<Integer> output3 = filterWithInstanceOf(Functional.isOdd, input);
+
+        System.out.println("Size of output1 (Iterable) is "+output1.size());
+        System.out.println("Size of output2 (Collection) is "+output2.size());
+        System.out.println("Size of output3 (instanceof) is "+output3.size());
+    }
+
+    @Test
+    public void filterTest2()
+    {
+        final int howMany=10;
+        final Collection<Integer> input = Functional.init(Functional.<Integer>identity(), 10000000);
+
+        {
+            final long start = System.currentTimeMillis();
+            for (int i = 0; i < howMany; ++i) {
+                final Collection<Integer> output = filterWithIterable(Functional.isOdd, input);
+            }
+            final long howLong = System.currentTimeMillis() - start;
+            System.out.println(howLong + "ms to filter using Iterable and whatever array resizing the JVM implements");
+        }
+        {
+            final long start = System.currentTimeMillis();
+            for (int i = 0; i < howMany; ++i) {
+                final Collection<Integer> output = filterWithCollection(Functional.isOdd, input);
+            }
+            final long howLong = System.currentTimeMillis() - start;
+            System.out.println(howLong + "ms to filter using Collection");
+        }
+        {
+            final long start = System.currentTimeMillis();
+            for (int i = 0; i < howMany; ++i) {
+                final Collection<Integer> output = filterWithInstanceOf(Functional.isOdd, input);
+            }
+            final long howLong = System.currentTimeMillis() - start;
+            System.out.println(howLong + "ms to filter using a Collection and instanceof to determine the initial array size");
+        }
+
+        final Collection<Integer> output1 = filterWithIterable(Functional.isOdd, input);
+        final Collection<Integer> output2 = filterWithCollection(Functional.isOdd, input);
+        final Collection<Integer> output3 = filterWithInstanceOf(Functional.isOdd, input);
+
+        System.out.println("Size of output1 (Iterable) is "+output1.size());
+        System.out.println("Size of output2 (Collection) is "+output2.size());
+        System.out.println("Size of output3 (instanceof) is "+output3.size());
+    }
+}
