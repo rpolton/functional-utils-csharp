c2d10068f5cfb491dbf7941d5c097e41620d1c4f
Richard.of.Shaftesbury@gmail.com <Richard.of.Shaftesbury@gmail.com@052c0c06-60dd-4d73-2e48-45615e194d6a>
2014-09-17 12:22:27 +0000

    Add another test to the filter to verify that creating a half-size array up-front is or isn't an improvement. Split the pieces of the tests into separately executed functions so that the memory usage doesn't impact on the subsequent timings.

--- Functional-Utils-Java/src/test/java/me/shaftesbury/utils/functional/SpeedTest.java
+++ Functional-Utils-Java/src/test/java/me/shaftesbury/utils/functional/SpeedTest.java
@@ -91,6 +91,16 @@ public class SpeedTest
         return Collections.unmodifiableList(output);
     }
 
+    public final static <A>List<A> filterWithHalfCollection(final Func<? super A,Boolean> pred, final Collection<A> input)
+    {
+        final List<A> output = new ArrayList<A>((input.size()/2)+1);
+        for(final A element : input)
+            if(pred.apply(element))
+                output.add(element);
+
+        return Collections.unmodifiableList(output);
+    }
+
     public final static <A>List<A> filterWithInstanceOf(final Func<? super A,Boolean> pred, final Iterable<A> input)
     {
         final List<A> output = input instanceof Collection<?> ? new ArrayList<A>(((Collection) input).size()) : new ArrayList<A>();
@@ -102,7 +112,7 @@ public class SpeedTest
     }
 
     @Test
-    public void filterTest1()
+    public void filterTestWithEmptyResultsAndIterable()
     {
         final int howMany=10;
         final Collection<Integer> input = Functional.init(DoublingGenerator, 10000000);
@@ -115,6 +125,18 @@ public class SpeedTest
             final long howLong = System.currentTimeMillis() - start;
             System.out.println(howLong + "ms to filter using Iterable and whatever array resizing the JVM implements");
         }
+
+        final Collection<Integer> output1 = filterWithIterable(Functional.isOdd, input);
+
+        System.out.println("Size of output1 (Iterable) is "+output1.size());
+    }
+
+    @Test
+    public void filterTestWithEmptyResultsAndCollection()
+    {
+        final int howMany=10;
+        final Collection<Integer> input = Functional.init(DoublingGenerator, 10000000);
+
         {
             final long start = System.currentTimeMillis();
             for (int i = 0; i < howMany; ++i) {
@@ -123,6 +145,18 @@ public class SpeedTest
             final long howLong = System.currentTimeMillis() - start;
             System.out.println(howLong + "ms to filter using Collection");
         }
+
+        final Collection<Integer> output2 = filterWithCollection(Functional.isOdd, input);
+
+        System.out.println("Size of output2 (Collection) is "+output2.size());
+    }
+
+    @Test
+    public void filterTestWithEmptyResultsAndInstanceOf()
+    {
+        final int howMany=10;
+        final Collection<Integer> input = Functional.init(DoublingGenerator, 10000000);
+
         {
             final long start = System.currentTimeMillis();
             for (int i = 0; i < howMany; ++i) {
@@ -132,17 +166,33 @@ public class SpeedTest
             System.out.println(howLong + "ms to filter using a Collection and instanceof to determine the initial array size");
         }
 
-        final Collection<Integer> output1 = filterWithIterable(Functional.isOdd, input);
-        final Collection<Integer> output2 = filterWithCollection(Functional.isOdd, input);
         final Collection<Integer> output3 = filterWithInstanceOf(Functional.isOdd, input);
 
-        System.out.println("Size of output1 (Iterable) is "+output1.size());
-        System.out.println("Size of output2 (Collection) is "+output2.size());
         System.out.println("Size of output3 (instanceof) is "+output3.size());
     }
 
     @Test
-    public void filterTest2()
+    public void filterTestWithEmptyResultsAndHalfSizeCollection()
+    {
+        final int howMany=10;
+        final Collection<Integer> input = Functional.init(DoublingGenerator, 10000000);
+
+        {
+            final long start = System.currentTimeMillis();
+            for (int i = 0; i < howMany; ++i) {
+                final Collection<Integer> output = filterWithHalfCollection(Functional.isOdd, input);
+            }
+            final long howLong = System.currentTimeMillis() - start;
+            System.out.println(howLong + "ms to filter using a Collection initialised to half-size to determine the initial array size");
+        }
+
+        final Collection<Integer> output4 = filterWithHalfCollection(Functional.isOdd, input);
+
+        System.out.println("Size of output4 (Collection half-size) is "+output4.size());
+    }
+
+    @Test
+    public void filterTestWithHalfSizeResultsAndIterable()
     {
         final int howMany=10;
         final Collection<Integer> input = Functional.init(Functional.<Integer>identity(), 10000000);
@@ -155,6 +205,18 @@ public class SpeedTest
             final long howLong = System.currentTimeMillis() - start;
             System.out.println(howLong + "ms to filter using Iterable and whatever array resizing the JVM implements");
         }
+
+        final Collection<Integer> output1 = filterWithIterable(Functional.isOdd, input);
+
+        System.out.println("Size of output1 (Iterable) is "+output1.size());
+    }
+
+    @Test
+    public void filterTestWithHalfSizeResultsAndCollection()
+    {
+        final int howMany=10;
+        final Collection<Integer> input = Functional.init(Functional.<Integer>identity(), 10000000);
+
         {
             final long start = System.currentTimeMillis();
             for (int i = 0; i < howMany; ++i) {
@@ -163,6 +225,18 @@ public class SpeedTest
             final long howLong = System.currentTimeMillis() - start;
             System.out.println(howLong + "ms to filter using Collection");
         }
+
+        final Collection<Integer> output2 = filterWithCollection(Functional.isOdd, input);
+
+        System.out.println("Size of output2 (Collection) is "+output2.size());
+    }
+
+    @Test
+    public void filterTestWithHalfSizeResultsAndInstanceOf()
+    {
+        final int howMany=10;
+        final Collection<Integer> input = Functional.init(Functional.<Integer>identity(), 10000000);
+
         {
             final long start = System.currentTimeMillis();
             for (int i = 0; i < howMany; ++i) {
@@ -172,12 +246,28 @@ public class SpeedTest
             System.out.println(howLong + "ms to filter using a Collection and instanceof to determine the initial array size");
         }
 
-        final Collection<Integer> output1 = filterWithIterable(Functional.isOdd, input);
-        final Collection<Integer> output2 = filterWithCollection(Functional.isOdd, input);
         final Collection<Integer> output3 = filterWithInstanceOf(Functional.isOdd, input);
 
-        System.out.println("Size of output1 (Iterable) is "+output1.size());
-        System.out.println("Size of output2 (Collection) is "+output2.size());
         System.out.println("Size of output3 (instanceof) is "+output3.size());
+    }
+
+    @Test
+    public void filterTestWithHalfSizeResultsAndHalfSizeCollection()
+    {
+        final int howMany=10;
+        final Collection<Integer> input = Functional.init(Functional.<Integer>identity(), 10000000);
+
+        {
+            final long start = System.currentTimeMillis();
+            for (int i = 0; i < howMany; ++i) {
+                final Collection<Integer> output = filterWithHalfCollection(Functional.isOdd, input);
+            }
+            final long howLong = System.currentTimeMillis() - start;
+            System.out.println(howLong + "ms to filter using a Collection initialised to half-size to determine the initial array size");
+        }
+
+        final Collection<Integer> output4 = filterWithHalfCollection(Functional.isOdd, input);
+
+        System.out.println("Size of output4 (Collection half-size) is "+output4.size());
     }
 }
